bfa71ca72922b56dca6713e2e32c2f8a3e3dee6a
ningi265 <ningintonga@gmail.com>
2024-01-06 23:55:06 -0800

    1

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list[])
     for num in my_list:
-        print("{}".format(num))
-my_list = [1, 2, 3, 4]
-print_list_integer(my_list)
+        print("{:d}".format(num))
