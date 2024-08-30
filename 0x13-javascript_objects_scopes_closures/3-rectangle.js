79c3fdc3e0222e85bc8a8df71b8555c55fd19b87
legendmw <brianmtonga592@gmail.com>
2024-08-30 07:57:46 +0200

    3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 /** a class Rectangle that defines a rectangle */
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
   
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        let rec = [];
-        for (let j = 0; j < this.width; j++) {
-          rec += 'X';
-        }
-        console.log(rec);
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rec = [];
+      for (let j = 0; j < this.width; j++) {
+        rec += 'X';
       }
+      console.log(rec);
     }
-  };
+  }
+};
