74716769808c44014289226d4feceda869979f06
legendmw <brianmtonga592@gmail.com>
2024-08-30 07:42:59 +0200

    7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 /* returns the number of occurences in a list */
 exports.nbOccurences = function (list, searchElement) {
-    let repeat = 0;
-    for (let i = 0; i < list.length; i++) {
-      if (list[i] === searchElement) {
-        repeat += 1;
-      }
+  let repeat = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      repeat += 1;
     }
-    return repeat;
-  };
+  }
+  return repeat;
+};
