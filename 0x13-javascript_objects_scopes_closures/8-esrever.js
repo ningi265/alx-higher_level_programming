b68484c8d53bbde8a67c035b220eb09aea4dbf87
legendmw <brianmtonga592@gmail.com>
2024-08-30 07:41:17 +0200

    8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /* returns the reversed version of a list */
 exports.esrever = function (list) {
-    const newList = [];
-    for (let i = list.length - 1; i > -1; i--) {
-      newList.push(list[i]);
-    }
-    return newList;
-  };
+  const newList = [];
+  for (let i = list.length - 1; i > -1; i--) {
+    newList.push(list[i]);
+  }
+  return newList;
+};
