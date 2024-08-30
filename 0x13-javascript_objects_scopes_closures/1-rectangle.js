2a1e5cfa8be05dd4b95dc349eb4e34bc2262a8a4
legendmw <brianmtonga592@gmail.com>
2024-08-30 07:31:12 +0200

    all

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/** a class Rectangle that defines a rectangle */
+module.exports = class Rectangle {
+    width;
+    height;
+  
+    constructor (w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  };
---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+/** a class Rectangle that defines a rectangle */
+module.exports = class Rectangle {
+    #temp;
+  
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  
+    print () {
+      for (let i = 0; i < this.height; i++) {
+        let rec = [];
+        for (let j = 0; j < this.width; j++) {
+          rec += 'X';
+        }
+        console.log(rec);
+      }
+    }
+  
+    rotate () {
+      this.#temp = this.height;
+      this.height = this.width;
+      this.width = this.#temp;
+    }
+  
+    double () {
+      this.width = this.width * 2;
+      this.height = this.height * 2;
+    }
+  };
